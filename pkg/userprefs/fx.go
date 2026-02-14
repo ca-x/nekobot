ff1ae83f0b872c721470b7efa485d89054ed3b01
czyt <czytcn@gmail.com>
2026-02-14 12:33:06 +0800

    refactor(userprefs): store channel preferences in dedicated file

--- pkg/userprefs/fx.go
+++ pkg/userprefs/fx.go
@@ -1,8 +1,15 @@
 package userprefs
 
 import (
-	"go.uber.org/fx"
+	"context"
+	"path/filepath"
+	"time"
 
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+
+	"nekobot/pkg/config"
+	"nekobot/pkg/logger"
 	"nekobot/pkg/state"
 )
 
@@ -11,15 +18,27 @@ var Module = fx.Module("userprefs",
 	fx.Provide(provideManager),
 )
 
-type managerParams struct {
-	fx.In
+func provideManager(lc fx.Lifecycle, log *logger.Logger, cfg *config.Config) (*Manager, error) {
+	storePath := filepath.Join(cfg.WorkspacePath(), "userprefs.json")
 
-	Store state.KV `optional:"true"`
-}
-
-func provideManager(p managerParams) *Manager {
-	if p.Store == nil {
-		return nil
+	store, err := state.NewFileStore(log, &state.FileStoreConfig{
+		FilePath:     storePath,
+		AutoSave:     true,
+		SaveInterval: 2 * time.Second,
+	})
+	if err != nil {
+		return nil, err
 	}
-	return New(p.Store)
+
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			log.Info("User preferences store initialized", zap.String("path", storePath))
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return store.Close()
+		},
+	})
+
+	return New(store), nil
 }
